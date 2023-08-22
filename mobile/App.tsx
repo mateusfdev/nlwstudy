a14a3d18c257a20c5ff7c1d84e4690863fcd9a51
Your Name <you@example.com>
2023-08-22 08:16:05 -0300

    config mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className='text-gray-50 font-bold text-4xl'>
+        mobile server test 🔥🚀
+      </Text>
+      <StatusBar style="light" translucent/>
+    </View>
+  );
+}
+
